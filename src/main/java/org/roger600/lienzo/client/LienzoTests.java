7c7e873262f9d7d0f6fa1287ad55a883544b90f6
Roger Martinez <roger600@gmail.com>
2015-11-30 19:53:16 +0100

    Lienzo tests

---
+++ src/main/java/org/roger600/lienzo/client/LienzoTests.java
@@ -0,0 +1,32 @@
+package org.roger600.lienzo.client;
+
+import com.ait.lienzo.client.core.shape.Layer;
+import com.ait.lienzo.client.core.shape.Rectangle;
+import com.ait.lienzo.client.widget.LienzoPanel;
+import com.ait.lienzo.shared.core.types.ColorName;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class LienzoTests implements EntryPoint {
+
+    private FlowPanel mainPanel = new FlowPanel();
+    private LienzoPanel panel = new LienzoPanel(1200, 900);
+    private Layer layer = new Layer();
+    
+    public void onModuleLoad()
+    {
+        RootPanel.get().add(mainPanel);
+        mainPanel.add(panel);
+        layer.setTransformable(true);
+        panel.add(layer);
+
+        drawIt();
+        layer.draw();
+    }
+    
+    private void drawIt() {
+        layer.add(new Rectangle(100, 100).setFillColor(ColorName.BLACK));
+    }
+    
+}
