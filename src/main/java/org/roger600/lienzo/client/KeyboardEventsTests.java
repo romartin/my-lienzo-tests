37c8d550c8a26e85b71a67db1010944418942fb2
roger <roger600@gmail.com>
2016-08-01 03:02:51 +0200

    keyboard tests

--- src/main/java/org/roger600/lienzo/client/KeyboardEventsTests.java
+++ src/main/java/org/roger600/lienzo/client/KeyboardEventsTests.java
@@ -4,6 +4,8 @@ import com.ait.lienzo.client.core.shape.Layer;
 import com.ait.lienzo.client.core.shape.Rectangle;
 import com.ait.lienzo.client.widget.LienzoPanel;
 import com.ait.lienzo.shared.core.types.ColorName;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.*;
 
 public class KeyboardEventsTests {
 
@@ -14,27 +16,6 @@ public class KeyboardEventsTests {
     }
     
     public void test( final LienzoPanel lienzoPanel ) {
-       /*
-        lienzoPanel.addKeyPressHandler( new KeyPressHandler() {
-            @Override
-            public void onKeyPress( KeyPressEvent keyPressEvent ) {
-                GWT.log("PRESS="+keyPressEvent.getUnicodeCharCode());
-            }
-        } );
-
-        lienzoPanel.addKeyDownHandler( new KeyDownHandler() {
-            @Override
-            public void onKeyDown( KeyDownEvent keyDownEvent ) {
-                GWT.log( "DOWN=" + keyDownEvent.getNativeKeyCode() );
-            }
-        } );
-
-        lienzoPanel.addKeyUpHandler( new KeyUpHandler() {
-            @Override
-            public void onKeyUp( KeyUpEvent keyUpEvent ) {
-                GWT.log( "UP=" + keyUpEvent.getNativeKeyCode() );
-            }
-        } );*/
 
         Rectangle rectangle = new Rectangle( 400, 400 ).setFillColor( ColorName.BLACK );
 
--- src/main/java/org/roger600/lienzo/client/LienzoKeyboardTests.java
+++ src/main/java/org/roger600/lienzo/client/LienzoKeyboardTests.java
@@ -1,31 +1,132 @@
 package org.roger600.lienzo.client;
 
 import com.ait.lienzo.client.core.shape.Layer;
-import com.ait.lienzo.client.widget.LienzoPanel;
+import com.ait.lienzo.client.core.shape.Rectangle;
+import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.dom.client.*;
 import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import org.roger600.lienzo.client.panel.FocusableLienzoPanelView;
 
-public class LienzoKeyboardTests /*implements EntryPoint*/ {
+public class LienzoKeyboardTests implements EntryPoint {
 
-    private LienzoPanel panel = new LienzoPanel(1200, 900);
+    private FocusableLienzoPanelView panel = new FocusableLienzoPanelView( 1200,900 );
+    //private LienzoPanel panel = new LienzoPanel(1200, 900);
     private Layer layer = new Layer();
-    
+
+    private boolean listening = false;
+
     public void onModuleLoad()
     {
-        RootPanel.get().add(panel);
+        if ( false ) {
+
+            testGWTFocusPanel();
+
+        } else if ( false ) {
+
+            testWrapping();
+
+        } else {
+
+            testListenOnRootPanel();
+
+        }
+
+    }
+
+    private void testListenOnRootPanel() {
+
+        RootPanel.get().add( panel );
+
+        panel.addMouseOverHandler( new MouseOverHandler() {
+            @Override
+            public void onMouseOver( MouseOverEvent mouseOverEvent ) {
+                // GWT.log( "OVERRRR" );
+                LienzoKeyboardTests.this.listening = true;
+            }
+        } );
+
+        panel.addMouseOutHandler( new MouseOutHandler() {
+            @Override
+            public void onMouseOut( MouseOutEvent mouseOutEvent ) {
+                // GWT.log( "OUTT" );
+                LienzoKeyboardTests.this.listening = false;
+            }
+        } );
+
+        RootPanel.get().addDomHandler( new KeyPressHandler() {
+            @Override
+            public void onKeyPress( KeyPressEvent keyPressEvent ) {
+                if ( LienzoKeyboardTests.this.listening ) {
+                    GWT.log("PRESS="+keyPressEvent.getUnicodeCharCode());
+                }
+            }
+        }, KeyPressEvent.getType() );
+
+        RootPanel.get().addDomHandler( new KeyDownHandler() {
+            @Override
+            public void onKeyDown( KeyDownEvent keyDownEvent ) {
+                if ( LienzoKeyboardTests.this.listening ) {
+                    GWT.log( "DOWN=" + keyDownEvent.getNativeKeyCode() );
+                }
+            }
+        }, KeyDownEvent.getType() );
+
+        RootPanel.get().addDomHandler( new KeyUpHandler() {
+            @Override
+            public void onKeyUp( KeyUpEvent keyUpEvent ) {
+                if ( LienzoKeyboardTests.this.listening ) {
+                    GWT.log( "UP=" + keyUpEvent.getNativeKeyCode() );
+                }
+            }
+        }, KeyUpEvent.getType() );
+
         layer.setTransformable(true);
         panel.add(layer);
 
-        KeyboardEventsTests tests = new KeyboardEventsTests(layer);
-        tests.test(panel);
+        layer.add( new Rectangle( 50, 50 ) );
         layer.draw();
 
     }
 
-    private void testKeyH() {
+    private void testWrapping() {
+        FocusPanel focusPanel = new FocusPanel();
+        focusPanel.getElement().getStyle().setWidth( 100, Style.Unit.PC );
+        RootPanel.get().add(focusPanel);
+        focusPanel.add( panel );
+
+        focusPanel.addKeyPressHandler( new KeyPressHandler() {
+            @Override
+            public void onKeyPress( KeyPressEvent keyPressEvent ) {
+                GWT.log("PRESS="+keyPressEvent.getUnicodeCharCode());
+            }
+        } );
+
+        focusPanel.addKeyDownHandler( new KeyDownHandler() {
+            @Override
+            public void onKeyDown( KeyDownEvent keyDownEvent ) {
+                GWT.log( "DOWN=" + keyDownEvent.getNativeKeyCode() );
+            }
+        } );
+
+        focusPanel.addKeyUpHandler( new KeyUpHandler() {
+            @Override
+            public void onKeyUp( KeyUpEvent keyUpEvent ) {
+                GWT.log( "UP=" + keyUpEvent.getNativeKeyCode() );
+            }
+        } );
+
+
+        layer.setTransformable(true);
+        panel.add(layer);
+
+        layer.add( new Rectangle( 50, 50 ) );
+        layer.draw();
+    }
+
+    private void testGWTFocusPanel() {
 
         FocusPanel mainPanel = new FocusPanel();
         RootPanel.get().add(mainPanel);
