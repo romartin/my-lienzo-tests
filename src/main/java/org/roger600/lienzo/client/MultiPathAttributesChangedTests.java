a9d1ce70a59143bd588bf30db8b830c526dc1b18
roger <roger600@gmail.com>
2016-09-16 01:23:05 +0200

    multipath tests.

--- src/main/java/org/roger600/lienzo/client/MultiPathAttributesChangedTests.java
+++ src/main/java/org/roger600/lienzo/client/MultiPathAttributesChangedTests.java
@@ -7,6 +7,7 @@ import com.ait.lienzo.client.core.shape.MultiPath;
 import com.ait.lienzo.client.core.shape.Rectangle;
 import com.ait.lienzo.client.core.shape.wires.IControlHandle;
 import com.ait.lienzo.client.core.shape.wires.IControlHandleList;
+import com.ait.lienzo.client.core.types.BoundingBox;
 import com.ait.lienzo.shared.core.types.ColorName;
 import com.ait.tooling.common.api.flow.Flows;
 import com.google.gwt.core.client.GWT;
@@ -18,6 +19,8 @@ import static com.ait.lienzo.client.core.AttributeOp.any;
 public class MultiPathAttributesChangedTests implements MyLienzoTest {
 
     private static final Flows.BooleanOp XY_OP  = any( Attribute.X, Attribute.Y );
+    private static final Flows.BooleanOp WH_OP  = any( Attribute.WIDTH, Attribute.HEIGHT );
+    private static final Flows.BooleanOp POINTS_OP  = any( Attribute.PATH );
 
     private final IAttributesChangedBatcher attributesChangedBatcher = new AnimationFrameAttributesChangedBatcher();
     private IControlHandleList m_ctrls;
@@ -50,10 +53,24 @@ public class MultiPathAttributesChangedTests implements MyLienzoTest {
             @Override
             public void onAttributesChanged(AttributesChangedEvent event)
             {
+                GWT.log( "HANDLER FIRED -> " + event.toJSONString() );
+
                 if (event.evaluate(XY_OP))
                 {
                     GWT.log( "SHAPE MOVED TO [" + m_multi.getX() + ", "
                     + m_multi.getY() + "]!!");
+                }
+                if (event.evaluate(WH_OP))
+                {
+                    BoundingBox bb = m_multi.getBoundingBox();
+                    GWT.log( "SHAPE RESIZED TO [" + bb.getWidth() + ", "
+                            + bb.getHeight() + "]!!");
+                }
+                if (event.evaluate(POINTS_OP))
+                {
+                    BoundingBox bb = m_multi.getBoundingBox();
+                    GWT.log( "SHAPE PATH RESIZED TO [" + bb.getWidth() + ", "
+                            + bb.getHeight() + "]!!");
                 }
             }
         };
@@ -61,6 +78,16 @@ public class MultiPathAttributesChangedTests implements MyLienzoTest {
         // Attribute change handlers.
         m_multi.addAttributesChangedHandler(Attribute.X, handler);
         m_multi.addAttributesChangedHandler(Attribute.Y, handler);
+        m_multi.addAttributesChangedHandler(Attribute.WIDTH, handler);
+        m_multi.addAttributesChangedHandler(Attribute.HEIGHT, handler);
+        m_multi.addAttributesChangedHandler(Attribute.PATH, handler);
+
+
+        Attribute[] allAttrs = getAllAttributes();
+        for ( final Attribute a : allAttrs ) {
+            m_multi.addAttributesChangedHandler(a, handler);
+        }
+
     }
 
     private void addButtons( final double x, final double y ) {
@@ -131,5 +158,105 @@ public class MultiPathAttributesChangedTests implements MyLienzoTest {
             }
         });
     }
-    
+
+
+    private Attribute[] getAllAttributes() {
+        return new Attribute[] {
+                Attribute.HEIGHT,
+                Attribute.CORNER_RADIUS,
+                Attribute.FILL,
+                Attribute.STROKE,
+                Attribute.STROKE_WIDTH,
+                Attribute.LINE_JOIN,
+                Attribute.X,
+                Attribute.Y,
+                Attribute.VISIBLE,
+                Attribute.LISTENING,
+                Attribute.ID,
+                Attribute.NAME,
+                Attribute.ALPHA,
+                Attribute.FILL_ALPHA,
+                Attribute.STROKE_ALPHA,
+                Attribute.SCALE,
+                Attribute.ROTATION,
+                Attribute.OFFSET,
+                Attribute.SHEAR,
+                Attribute.TRANSFORM,
+                Attribute.DRAGGABLE,
+                Attribute.EDITABLE,
+                Attribute.FILL_SHAPE_FOR_SELECTION,
+                Attribute.FILL_BOUNDS_FOR_SELECTION,
+                Attribute.DRAG_CONSTRAINT,
+                Attribute.DRAG_BOUNDS,
+                Attribute.RADIUS,
+                Attribute.RADIUS_X,
+                Attribute.RADIUS_Y,
+                Attribute.CLEAR_LAYER_BEFORE_DRAW,
+                Attribute.TRANSFORMABLE,
+                Attribute.TEXT,
+                Attribute.FONT_SIZE,
+                Attribute.FONT_FAMILY,
+                Attribute.FONT_STYLE,
+                Attribute.POINTS,
+                Attribute.STAR_POINTS,
+                Attribute.LINE_CAP,
+                Attribute.DASH_ARRAY,
+                Attribute.DASH_OFFSET,
+                Attribute.SIDES,
+                Attribute.OUTER_RADIUS,
+                Attribute.INNER_RADIUS,
+                Attribute.SKEW,
+                Attribute.SHADOW,
+                Attribute.START_ANGLE,
+                Attribute.END_ANGLE,
+                Attribute.COUNTER_CLOCKWISE,
+                Attribute.CONTROL_POINTS,
+                Attribute.TEXT_BASELINE,
+                Attribute.TEXT_ALIGN,
+                Attribute.TEXT_UNIT,
+                Attribute.CLIPPED_IMAGE_WIDTH,
+                Attribute.CLIPPED_IMAGE_HEIGHT,
+                Attribute.CLIPPED_IMAGE_START_X,
+                Attribute.CLIPPED_IMAGE_START_Y,
+                Attribute.CLIPPED_IMAGE_DESTINATION_WIDTH,
+                Attribute.CLIPPED_IMAGE_DESTINATION_HEIGHT,
+                Attribute.SERIALIZATION_MODE,
+                Attribute.URL,
+                Attribute.LOOP,
+                Attribute.AUTO_PLAY,
+                Attribute.PLAYBACK_RATE,
+                Attribute.SHOW_POSTER,
+                Attribute.VOLUME,
+                Attribute.BASE_WIDTH,
+                Attribute.HEAD_WIDTH,
+                Attribute.ARROW_ANGLE,
+                Attribute.BASE_ANGLE,
+                Attribute.ARROW_TYPE,
+                Attribute.MITER_LIMIT,
+                Attribute.CURVE_FACTOR,
+                Attribute.ANGLE_FACTOR,
+                Attribute.LINE_FLATTEN,
+                Attribute.TOP_WIDTH,
+                Attribute.BOTTOM_WIDTH,
+                Attribute.IMAGE_SELECTION_MODE,
+                Attribute.DRAG_MODE,
+                Attribute.PATH,
+                Attribute.TICK_RATE,
+                Attribute.SPRITE_BEHAVIOR_MAP,
+                Attribute.SPRITE_BEHAVIOR,
+                Attribute.ACTIVE,
+                Attribute.VALUE,
+                Attribute.COLOR,
+                Attribute.MATRIX,
+                Attribute.INVERTED,
+                Attribute.GAIN,
+                Attribute.BIAS,
+                Attribute.HEAD_OFFSET,
+                Attribute.TAIL_OFFSET,
+                Attribute.CORRECTION_OFFSET,
+                Attribute.HEAD_DIRECTION,
+                Attribute.TAIL_DIRECTION,
+                Attribute.EVENT_PROPAGATION_MODE
+        };
+    }
 }
