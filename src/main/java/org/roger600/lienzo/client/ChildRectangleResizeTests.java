404458909455cada5c2e64ff0ff3a654ecddf056
roger <roger600@gmail.com>
2016-09-23 21:59:52 +0200

    Fix wires shape's controls positions.

--- src/main/java/org/roger600/lienzo/client/ChildRectangleResizeTests.java
+++ src/main/java/org/roger600/lienzo/client/ChildRectangleResizeTests.java
@@ -63,7 +63,7 @@ public class ChildRectangleResizeTests extends FlowPanel implements MyLienzoTest
 
                     }
 
-                    currentShape = create( 800, 400, 200, 200, "#CC00EE", "#FFFFFF" );
+                    currentShape = create( 800, 400, 200, 200, "#CC00EE", 1, "#FFFFFF" );
                     Rectangle circle1 = new Rectangle( 50, 50).setFillColor("#00CCCC").setDraggable(false);
                     currentShape.addChild(circle1, layout);
 
@@ -76,23 +76,23 @@ public class ChildRectangleResizeTests extends FlowPanel implements MyLienzoTest
     }
 
     private void testAllLayouts( final Layer layer) {
-        WiresShape shapeTop = create( 400, 50, 100, 100, "#CC0000", "#FFFFFF" );
+        WiresShape shapeTop = create( 400, 50, 100, 100, "#CC0000", 1, "#FFFFFF" );
         Rectangle circle1 = new Rectangle( 50, 50).setFillColor("#0000CC").setDraggable(false);
         shapeTop.addChild(circle1, WiresLayoutContainer.Layout.TOP);
 
-        WiresShape shapeLeft = create( 50, 200, 100, 100, "#00CC00", "#FFFFFF" );
+        WiresShape shapeLeft = create( 50, 200, 100, 100, "#00CC00", 1, "#FFFFFF" );
         Rectangle circle2 = new Rectangle( 50, 50).setFillColor("#0000CC").setDraggable(false);
         shapeLeft.addChild(circle2, WiresLayoutContainer.Layout.LEFT);
 
-        WiresShape shapeCenter = create( 400, 200, 100, 100, "#0000CC", "#FFFFFF" );
+        WiresShape shapeCenter = create( 400, 200, 100, 100, "#0000CC", 1, "#FFFFFF" );
         Rectangle circle3 = new Rectangle( 50, 50).setFillColor("#CCCCCC").setDraggable(false);
         shapeCenter.addChild(circle3, WiresLayoutContainer.Layout.CENTER);
 
-        final WiresShape shapeRight = create( 800, 200, 100, 100, "#0000FF", "#FFFFFF" );
+        final WiresShape shapeRight = create( 800, 200, 100, 100, "#0000FF", 1, "#FFFFFF" );
         final Rectangle circle4 = new Rectangle( 50, 50).setFillColor("#CC00CC").setDraggable(false);
         shapeRight.addChild(circle4, WiresLayoutContainer.Layout.RIGHT);
 
-        WiresShape shapeBottom = create( 400, 400, 100, 100, "#FF00FF", "#FFFFFF" );
+        WiresShape shapeBottom = create( 400, 400, 100, 100, "#FF00FF", 1, "#FFFFFF" );
         Rectangle circle5 = new Rectangle( 50, 50).setFillColor("#0000CC").setDraggable(false);
         shapeBottom.addChild(circle5, WiresLayoutContainer.Layout.BOTTOM);
 
@@ -117,10 +117,11 @@ public class ChildRectangleResizeTests extends FlowPanel implements MyLienzoTest
                                final double w,
                                final double h,
                                final String fillColor,
+                               final double fillAlpha,
                                final String strokeColor) {
 
         WiresShape endEventShape = new WiresShape( new MultiPath().rect( 0, 0, w, h )
-                .setStrokeColor( strokeColor ).setFillColor( fillColor ) )
+                .setStrokeColor( strokeColor ).setFillColor( fillColor ).setFillAlpha( fillAlpha ) )
                 .setX( x ).setY( y );
         wires_manager.register( endEventShape );
         wires_manager.getMagnetManager().createMagnets( endEventShape );
