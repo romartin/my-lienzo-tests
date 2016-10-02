d92edd3550b2625fc65580fa04f22ca9a3f418d1
roger <roger600@gmail.com>
2016-10-02 03:53:52 +0200

    tests

--- src/main/java/org/roger600/lienzo/client/StandaloneConnectorsTests.java
+++ src/main/java/org/roger600/lienzo/client/StandaloneConnectorsTests.java
@@ -2,12 +2,65 @@ package org.roger600.lienzo.client;
 
 import com.ait.lienzo.client.core.shape.*;
 import com.ait.lienzo.client.core.shape.wires.*;
+import com.ait.lienzo.client.core.shape.wires.handlers.WiresConnectorControl;
 import com.ait.lienzo.client.core.types.Point2DArray;
 import com.ait.lienzo.shared.core.types.ColorName;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Panel;
 
 
-public class StandaloneConnectorsTests extends FlowPanel implements MyLienzoTest, HasMediators {
+public class StandaloneConnectorsTests extends FlowPanel implements MyLienzoTest, HasMediators, HasButtons {
+
+    private WiresConnector c1;
+    private WiresConnectorControl control1;
+    private WiresConnector c2;
+    private WiresConnectorControl control2;
+    private WiresConnector c3;
+    private WiresConnectorControl control3;
+
+    @Override
+    public void setButtonsPanel( Panel panel ) {
+
+        final Button showMagnetsButton1 = new Button( "Show ctrls #|" );
+        showMagnetsButton1.addClickHandler( new ClickHandler() {
+            @Override
+            public void onClick( ClickEvent clickEvent ) {
+
+                control1.showControlPoints();
+
+            }
+        } );
+
+        panel.add( showMagnetsButton1 );
+
+        final Button showMagnetsButton2 = new Button( "Show ctrls #2" );
+        showMagnetsButton2.addClickHandler( new ClickHandler() {
+            @Override
+            public void onClick( ClickEvent clickEvent ) {
+
+                control2.showControlPoints();
+
+            }
+        } );
+
+        panel.add( showMagnetsButton2 );
+
+        final Button showMagnetsButton3 = new Button( "Show ctrls #3" );
+        showMagnetsButton3.addClickHandler( new ClickHandler() {
+            @Override
+            public void onClick( ClickEvent clickEvent ) {
+
+                control3.showControlPoints();
+
+            }
+        } );
+
+        panel.add( showMagnetsButton3 );
+
+    }
 
     public void test(Layer layer) {
 
@@ -51,13 +104,23 @@ public class StandaloneConnectorsTests extends FlowPanel implements MyLienzoTest
         task2NodeShape.setX(startX + 200).setY(startY + 300).getContainer().setUserData(ColorName.YELLOW.getColorString());
         wires_manager.getMagnetManager().createMagnets(task2NodeShape);
 
-        createConnector( layer, wires_manager, 0, 0, 100, 100 );
+        c1 = createConnector( layer, wires_manager, 0, 0, 100, 100 );
+        control1 = wires_manager.register( c1 );
 
-        createConnector( layer, wires_manager, 200, 200, 300, 300 );
+        c2 = createConnector( layer, wires_manager, 200, 200, 300, 300 );
+        control2 = wires_manager.register( c2 );
+
+        // Connector from blue start event to green task node.
+        c3 = connect(layer, startEventShape.getMagnets(), 3, taskNodeShape.getMagnets(), 7, wires_manager);
+        control3 = wires_manager.register( c3 );
 
     }
 
     private WiresConnector createConnector( Layer layer, WiresManager wiresManager, double... points ) {
+        return createConnector( layer, wiresManager, null, null, points );
+    }
+
+    private WiresConnector createConnector( Layer layer, WiresManager wiresManager, WiresMagnet headMagnet, WiresMagnet tailMagnet, double... points ) {
 
         MultiPath head = new MultiPath();
         head.M(15, 20);
@@ -73,11 +136,10 @@ public class StandaloneConnectorsTests extends FlowPanel implements MyLienzoTest
 
         OrthogonalPolyLine line = createLine( layer, points );
 
-        WiresConnector connector = new WiresConnector( null, null, line,
+        WiresConnector connector = new WiresConnector( headMagnet, tailMagnet, line,
                 new MultiPathDecorator(head),
                 new MultiPathDecorator(tail));
 
-        wiresManager.register( connector );
 
         head.setStrokeWidth(5).setStrokeColor("#0000CC");
         tail.setStrokeWidth(5).setStrokeColor("#0000CC");
@@ -86,46 +148,19 @@ public class StandaloneConnectorsTests extends FlowPanel implements MyLienzoTest
         return connector;
     }
 
-    private void connect(Layer layer, MagnetManager.Magnets magnets0, int i0_1, MagnetManager.Magnets magnets1, int i1_1, WiresManager wiresManager)
-    {
+    private WiresConnector connect(Layer layer, MagnetManager.Magnets magnets0, int i0_1, MagnetManager.Magnets magnets1, int i1_1, WiresManager wiresManager) {
 
-        WiresMagnet m0_1 = magnets0.getMagnet(i0_1);
-        WiresMagnet m1_1 = magnets1.getMagnet(i1_1);
+        WiresMagnet m0_1 = magnets0.getMagnet( i0_1 );
+        WiresMagnet m1_1 = magnets1.getMagnet( i1_1 );
 
         double x0, x1, y0, y1;
-
-        MultiPath head = new MultiPath();
-        head.M(15, 20);
-        head.L(0, 20);
-        head.L(15/2, 0);
-        head.Z();
-
-        MultiPath tail = new MultiPath();
-        tail.M(15, 20);
-        tail.L(0, 20);
-        tail.L(15/2, 0);
-        tail.Z();
-
-        OrthogonalPolyLine line;
         x0 = m0_1.getControl().getX();
         y0 = m0_1.getControl().getY();
         x1 = m1_1.getControl().getX();
         y1 = m1_1.getControl().getY();
-        line = createLine(layer, 0, 0, x0, y0, (x0 + ((x1-x0)/2)), (y0 + ((y1-y0)/2)), x1, y1);
-        final double hh = head.getBoundingBox().getHeight();
-        line.setHeadOffset( hh );
-        final double th = tail.getBoundingBox().getHeight();
-        line.setTailOffset( th );
 
-        WiresConnector connector = new WiresConnector(m0_1, m1_1, line,
-                new MultiPathDecorator(head),
-                new MultiPathDecorator(tail));
+        return createConnector( layer, wiresManager,  m0_1, m1_1, 0, 0, x0, y0, (x0 + ((x1-x0)/2)), (y0 + ((y1-y0)/2)), x1, y1 );
 
-        wiresManager.register( connector );
-
-        head.setStrokeWidth(5).setStrokeColor("#0000CC");
-        tail.setStrokeWidth(5).setStrokeColor("#0000CC");
-        line.setStrokeWidth(5).setStrokeColor("#0000CC");
     }
 
     private final OrthogonalPolyLine createLine( final Layer layer,
@@ -135,5 +170,6 @@ public class StandaloneConnectorsTests extends FlowPanel implements MyLienzoTest
         layer.add( line );
         return line;
     }
+
 
 }
