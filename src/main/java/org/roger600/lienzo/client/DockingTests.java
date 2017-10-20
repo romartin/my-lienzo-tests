0cf7e9bf83018a04a139b09c95ec666bedef3b41
josephblt <josephblt@gmail.com>
2017-10-20 12:09:40 -0200

    Min Max Constraints Support - Issue #121 Round Corners Support - Issue #140

--- src/main/java/org/roger600/lienzo/client/DockingTests.java
+++ src/main/java/org/roger600/lienzo/client/DockingTests.java
@@ -47,7 +47,7 @@ public class DockingTests extends FlowPanel implements MyLienzoTest, HasMediator
                 return true;
             }
         });
-        
+
         wires_manager.setDockingAcceptor(new IDockingAcceptor() {
             @Override
             public boolean dockingAllowed(WiresContainer parent, WiresShape child) {
@@ -105,41 +105,46 @@ public class DockingTests extends FlowPanel implements MyLienzoTest, HasMediator
             }
         });
 
-        MultiPath parentMultiPath = new MultiPath().rect(0, 0, 300, 300).setStrokeColor("#000000");
+        MultiPath parentMultiPath = new MultiPath().rect(0, 0, 200, 200).setStrokeColor("#000000");
         final WiresShape parentShape = new WiresShape(parentMultiPath);
         parentShape.getContainer().setUserData( "dock-source" );
         wires_manager.register( parentShape );
-        parentShape.setDraggable(true).setX(200).setY(200);
+        parentShape.setDraggable(true).setX(100).setY(200);
         wires_manager.getMagnetManager().createMagnets(parentShape);
+
+        MultiPath parentMultiPath2 = new MultiPath().rect(0, 0, 200, 200).setCornerRadius(30).setStrokeColor("#000000");
+        final WiresShape parentShape2 = new WiresShape(parentMultiPath2);
+        parentShape2.getContainer().setUserData( "dock-source" );
+        wires_manager.register( parentShape2 );
+        parentShape2.setDraggable(true).setX(500).setY(200);
+        wires_manager.getMagnetManager().createMagnets(parentShape2);
 
         MultiPath childMultiPath = new MultiPath().rect(0, 0, 100, 100).setStrokeColor("#CC0000");
         final WiresShape childShape = new WiresShape(childMultiPath);
         childShape.getContainer().setUserData( "dock-source" );
         wires_manager.register( childShape );
-        childShape.setDraggable(true).setX(600).setY(200);
+        childShape.setDraggable(true).setX(200).setY(200);
         wires_manager.getMagnetManager().createMagnets(childShape);
 
         MultiPath dockMultiPath = new MultiPath().rect(0, 0, 100, 100).setStrokeColor("#0000FF");
         final WiresShape dockShape = new WiresShape(dockMultiPath);
         dockShape.getContainer().setUserData( "dock-target" );
         wires_manager.register( dockShape );
-        dockShape.setDraggable(true).setX(600).setY(200);
+        dockShape.setDraggable(true).setX(200).setY(200);
         wires_manager.getMagnetManager().createMagnets(dockShape);
 
         doDock( parentShape, dockShape );
-        
     }
-    
+
     private void doDock( WiresShape parentShape , WiresShape child ) {
 
         child.removeFromParent();
 
         child.getGroup().setX( -50 ).setY( 50 );
-        
+
         parentShape.add(child);
 
         child.setDockedTo(parentShape);
-        
-    }
 
+    }
 }
