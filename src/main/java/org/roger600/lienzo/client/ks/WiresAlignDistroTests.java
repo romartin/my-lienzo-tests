e33e912e7904e7293b824f830cafdd6a8ddce190
Daniel J dos Santos <djdossantos@live.com>
2018-04-08 21:13:39 -0300

    Some WiresAlign tests, since it was not working.

--- src/main/java/org/roger600/lienzo/client/ks/WiresAlignDistroTests.java
+++ src/main/java/org/roger600/lienzo/client/ks/WiresAlignDistroTests.java
@@ -2,88 +2,66 @@ package org.roger600.lienzo.client.ks;
 
 import com.ait.lienzo.client.core.shape.*;
 import com.ait.lienzo.client.core.shape.wires.AlignAndDistribute;
+import com.ait.lienzo.client.core.shape.wires.WiresManager;
+import com.ait.lienzo.client.core.shape.wires.WiresShape;
+import com.ait.lienzo.client.core.types.Point2D;
+import com.ait.lienzo.shared.core.types.Color;
 import com.ait.lienzo.shared.core.types.ColorName;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Panel;
+
+import org.roger600.lienzo.client.HasButtons;
 import org.roger600.lienzo.client.MyLienzoTest;
+import org.roger600.lienzo.client.casemodeller.CaseModellerActivityShape;
+import org.roger600.lienzo.client.casemodeller.CaseModellerStageShape;
 
-// TODO: Not working??
+
 public class WiresAlignDistroTests extends FlowPanel implements MyLienzoTest {
-
-    public void test( final Layer layer ) {
-
-        Rectangle rect1 = new Rectangle(100, 100);
-        rect1.setDraggable(true);
-        rect1.setX(100);
-        rect1.setY(300);
-        rect1.setStrokeWidth(2);
-        rect1.setFillColor("#CC0000");
-        rect1.setFillAlpha(0.75);
-        rect1.setStrokeColor( ColorName.BLACK);
-        layer.add(rect1);
-
-        Circle circ1 = new Circle(50);
-        circ1.setDraggable(true);
-        circ1.setX(320);
-        circ1.setY(325);
-        circ1.setStrokeWidth(2);
-        circ1.setFillColor("#00CC00");
-        circ1.setFillAlpha(0.75);
-        circ1.setStrokeColor(ColorName.BLACK);
-        layer.add(circ1);
-
-        Rectangle rect3 = new Rectangle(100, 100);
-        rect3.setDraggable(true);
-        rect3.setX(500);
-        rect3.setY(250);
-        rect3.setStrokeWidth(2);
-        rect3.setFillColor("#AACC00");
-        rect3.setFillAlpha(0.75);
-        rect3.setStrokeColor(ColorName.BLACK);
-        layer.add(rect3);
-
-        Rectangle rect4 = new Rectangle(300, 150);
-        rect4.setCornerRadius(8);
-        rect4.setDraggable(true);
-        rect4.setX(50);
-        rect4.setY(50);
-        rect4.setStrokeWidth(2);
-        rect4.setFillColor("#55CCAA");
-        rect4.setFillAlpha(0.75);
-        rect4.setStrokeColor(ColorName.BLACK);
-        layer.add(rect4);
-
-        Text text1 = new Text("Align");
-        text1.setDraggable(true);
-        text1.setX(500);
-        text1.setY(500);
-        text1.setFontSize(96);
-        text1.setStrokeWidth(2);
-        text1.setFillColor(ColorName.HOTPINK);
-        text1.setFontStyle("bold");
-        text1.setFillAlpha(0.75);
-        text1.setStrokeColor(ColorName.BLACK);
-        layer.add(text1);
-
-        Star star1 = new Star(5, 50, 100);
-        star1.setDraggable(true);
-        star1.setX(250);
-        star1.setY(550);
-        star1.setStrokeWidth(2);
-        star1.setFillColor(ColorName.DARKORCHID);
-        star1.setFillAlpha(0.75);
-        star1.setStrokeColor(ColorName.BLACK);
-        layer.add(star1);
-
-        AlignAndDistribute index = new AlignAndDistribute(layer);
-        index.setStrokeWidth(2);
-        index.setStrokeColor(ColorName.DARKBLUE.getValue());
-        index.addShape(rect1);
-        index.addShape(circ1);
-        index.addShape(rect3);
-        index.addShape(rect4);
-        index.addShape(text1);
-        index.addShape(star1);
-
-    }
     
+	public void test(final Layer layer) {
+		
+		WiresManager wiresManager = WiresManager.get(layer);
+	
+		MultiPath circle = new MultiPath();				
+		circle.M(100, 0);
+        circle.A(150, 0, 150, 50, 50);
+        circle.A(150, 100, 100, 100, 50);
+        circle.A(50, 100, 50, 50, 50);
+        circle.A(50, 0, 100, 0, 50);
+        circle.Z();
+        circle.setDraggable(true);        
+        circle.setStrokeColor("#CC0000");
+        circle.setFillColor(ColorName.ORANGE);
+		WiresShape shape = new WiresShape(circle);
+		shape.setDraggable(true);
+		
+		wiresManager.register(shape);
+		
+		WiresShape rect1 = new WiresShape(new MultiPath().rect(0, 0, 50, 100).setStrokeColor("#CC0000"));
+		rect1.setDraggable(true);
+		wiresManager.register(rect1);
+		
+		WiresShape rect2 = new WiresShape(new MultiPath().rect(120, 20, 100, 100).setStrokeColor("#CC0000"));
+		rect2.setDraggable(true);
+		wiresManager.register(rect2);
+		
+		MultiPath star = new MultiPath();
+		star.M(100, 100);
+		star.L(200, 150);
+		star.L(300, 100);
+		star.L(250, 200);
+		star.L(300, 300);
+		star.L(200, 250);
+		star.L(100, 300);
+		star.L(150, 200);
+		star.Z();
+		star.setStrokeColor("#0000CC");
+		star.setDraggable(true);		
+		WiresShape starShape = new WiresShape(star);
+		starShape.setDraggable(true);
+		wiresManager.register(starShape);
+	}	
 }
